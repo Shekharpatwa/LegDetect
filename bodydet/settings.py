73d099fae87112a251d1efac4d7f240f8106f37d
ShekharPatwa <shekharpatwa2001@gmail.com>
2022-04-09 12:06:18 +0530

    Created Environment

--- bodydet/settings.py
+++ bodydet/settings.py
@@ -183,5 +183,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER ='shekharpatwa2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'patwa@1201'
+EMAIL_HOST_USER = os.environ.get('EM_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EM_PASS')
