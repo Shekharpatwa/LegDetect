1b2b8ffecbb8a79e53754c45c7fe164583569524
ShekharPatwa <shekharpatwa2001@gmail.com>
2022-04-09 15:53:19 +0530

    Done Designing 95%

--- bodydet/settings.py
+++ bodydet/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SEC_KEY')
+SECRET_KEY = 'django-insecure-zj=0l)9laaed)&mx(02%(8gkhpdqd@9rlg%8i23@8yz8#(1!7b'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
